9127d6042526e24f85a4dd5989ce00467de1228c
Ethan Seguin <ethan.seguin@techhigh.us>
2015-08-25 21:38:02 -0400

    Apples

---
+++ apples.java
@@ -0,0 +1,16 @@
+
+ class apples {
+public static void main (String args[])
+{
+	
+	int test = 6;
+if(test < 9)
+{
+System.out.println("Yes");
+}	
+else
+{
+System.out.println("this is else");	}
+}
+}
+
